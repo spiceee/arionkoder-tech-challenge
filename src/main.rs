c848ef57953ff9e63196c3cb49dd5e7eee8ce4c9
Fabio Mont Alegre <spiceee@gmail.com>
2025-08-14 12:25:36 -0300

    feat: Add basic routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused_imports)]
+#![deny(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
+
+async fn health_check() -> impl Responder {
+    let status = serde_json::json!({ "status": "OK" });
+    HttpResponse::Ok().json(status)
+}
+
+async fn messages() -> impl Responder {
+    let status = serde_json::json!({ "success": true });
+    HttpResponse::Ok().json(status)
+}
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/messages", web::post().to(messages))
+            .service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
